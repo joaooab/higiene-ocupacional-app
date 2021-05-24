500db352b3e6254829c85c95d4c40da5081334e2
joaooab <joaoovfreitas@gmail.com>
2021-05-24 14:58:47 -0300

    Change design and add splash screen

---
+++ app/src/main/java/br/com/joaoov/ui/splash/SplashFragment.kt
@@ -0,0 +1,27 @@
+package br.com.joaoov.ui.splash
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import br.com.joaoov.ComponentViewModel
+import br.com.joaoov.Components
+import br.com.joaoov.R
+import org.koin.android.viewmodel.ext.android.sharedViewModel
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    private val componentViewModel: ComponentViewModel by sharedViewModel()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        componentViewModel.withComponents = Components(path = false, toolbar = false)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val direction = SplashFragmentDirections.actionSplashFragmentToCompanyListFragment()
+            findNavController().navigate(direction)
+        }, 3000)
+    }
+
+}
