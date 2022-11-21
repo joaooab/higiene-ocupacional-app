f00c1eaadcfc502ff74c2023cb0bc7b08158fb7b
joaooab <joaoovfreitas@gmail.com>
2022-11-21 10:06:34 -0300

    refactor: remove unused code

--- app/src/main/java/br/com/joaoov/data/local/billing/BillingPlan.kt
+++ app/src/main/java/br/com/joaoov/data/local/billing/BillingPlan.kt
@@ -1,8 +1,5 @@
 package br.com.joaoov.data.local.billing
 
-import br.com.joaoov.Session
-import br.com.joaoov.data.remote.user.isLegalEntity
-
 data class BillingPlan(
     val productId: String,
     val name: String,
@@ -10,16 +7,6 @@ data class BillingPlan(
     val price: Long,
     val deleted: Boolean
 )
-
-fun BillingPlan.isEntityPlan() = productId.contains("company")
-
-fun BillingPlan.isUserPlan() = productId.contains("user")
-
-fun List<BillingPlan>.filterByUserType() = if (Session.user.isLegalEntity()) {
-    filter { it.isEntityPlan() }
-} else {
-    filter { it.isUserPlan() }
-}
 
 fun List<BillingPlan>.toLocal() = map { it.toLocal() }
 
